b5551c56194551d6ca88d04f5e66f05d18c00ef1
VINICIUS RODRIGUES <viniciusalro@gmail.com>
2025-03-18 21:05:26 -0300

    Primeiro commit do desenvolvimento do desafio ContaBanco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        int numeroConta;
+        String agenciaConta;
+        String nomeClienteConta;
+        double saldoConta;
+
+        System.out.println("***Bem vindo a criação da conta***");
+
+        System.out.println("Por favor, digite o nome do cliente: ");
+        nomeClienteConta = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o número da agência: ");
+        agenciaConta = scanner.next();
+
+        System.out.println("Por favor, digite o número da conta: ");
+        numeroConta = scanner.nextInt();        
+
+        System.out.println("Por favor, digite o saldo do cliente: ");
+        saldoConta = scanner.nextDouble();
+
+        System.out.println("Olá "+nomeClienteConta+", obrigado por criar uma conta em nosso banco, sua agência é "+agenciaConta+", conta "+numeroConta+" e seu saldo "+saldoConta+" já está disponível para saque");
+
+        scanner.close();
     }
+    
 }
